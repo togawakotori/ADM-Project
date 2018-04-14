d6d5105f25edcaff8ecc542930a2d39cb56434f7
togawakotori <Das_Elliot@163.com>
2018-04-14 06:01:05 +0000

    Add files via upload
    Backup for Matrix Multiplication
    DO NOT USE THIS PROGRAM

---
+++ matrix_v3.c
@@ -0,0 +1,257 @@
+#include <stdio.h>
+#include <malloc.h>
+#include <mpi.h>
+#include <math.h>// To compile with math.h use: mpicc prime.c -o prime -lm
+#include <stdlib.h>
+
+
+#define MIN(a,b) (a<b?a:b)
+#define MAX(a,b) (a<b?b:a)
+#define BLOCK_LOW(id,p,n) MAX((id*n)/p,0)//this expression replaces (id*n)/p to fix a serious bug: while p=1, BLOCK_LOW=-1.
+#define BLOCK_HIGH(id,p,n) (BLOCK_LOW((id+1),p,n)-1)
+#define BLOCK_SIZE(id,p,n) (BLOCK_LOW((id+1),p,n)-BLOCK_LOW(id,p,n))
+#define BLOCK_OWNER(index,p,n) (p*(index+1)-1)/n 
+
+#define VERSION 3//-v
+#define MAX_PROCESS(x,y) (VERSION>=2?MIN(x,y):x)
+
+#define DEBUG 1
+
+void mono(float **, int,int, float **,int,int);
+int malloc2dfloat(float ***,int,int);
+int free2dfloat(float ***);
+
+int main(int argc, char* argv[])
+{
+double elapsed_time;
+int id,p;
+int m_a=0,n_a=0,m_b=0,n_b=0,i,j;
+
+MPI_Init(&argc, &argv);
+MPI_Barrier(MPI_COMM_WORLD);
+elapsed_time = -MPI_Wtime();
+MPI_Comm_rank(MPI_COMM_WORLD,&id);
+MPI_Comm_size(MPI_COMM_WORLD,&p);
+
+int p_sqrt=(int)sqrt(p);
+#ifdef DEBUG
+printf("p_sqrt %d\n",p_sqrt);
+#endif
+
+if (!id) printf("VERSION %d\n",VERSION);
+FILE *fin;
+
+if (argc!=2){
+   if (!id) {printf("PLEASE INPUT COMMAND LINE: %s <FILE> \n",argv[0]);fflush(stdout);}
+   MPI_Finalize();
+   exit(1);
+}
+
+if (!id){fin=fopen(argv[1],"r");fscanf(fin,"%d %d\n",&m_a,&n_a);}
+
+MPI_Bcast(&m_a,1,MPI_INT,0,MPI_COMM_WORLD);
+MPI_Bcast(&n_a,1,MPI_INT,0,MPI_COMM_WORLD);
+
+if (MAX_PROCESS(m_a,n_a)<p_sqrt) {  
+   if (!id) {printf("FATAL ERROR: TOO MANY PROCESS\n");fflush(stdout);fclose(fin);}
+   MPI_Finalize();
+   exit(1);
+}
+
+MPI_Barrier(MPI_COMM_WORLD);//------------------------------------------------------------------
+
+float **a;malloc2dfloat(&a,m_a,n_a);
+float **a_local;malloc2dfloat(&a_local,BLOCK_SIZE(id,p,m_a),n_a);
+ 
+float **a_chip;malloc2dfloat(&a_chip,BLOCK_SIZE((int)id/p_sqrt,p_sqrt,m_a),BLOCK_SIZE(id%p_sqrt,p_sqrt,n_a));//eg. p=16, p_sqrt=4, id=15 id/p_sqrt=3 id%p_sqrt=3
+
+float **a_buffer[p];// a[p/p_sqrt][p%p_sqrt]
+if (!id)
+for (i=0;i<p;i++)
+{
+malloc2dfloat(&a_buffer[i],BLOCK_SIZE((int)i/p_sqrt,p_sqrt,m_a),BLOCK_SIZE(i%p_sqrt,p_sqrt,n_a));
+#ifdef DEBUG
+printf("ALLOCATE PROCESS %d SPACE %d*%d\n",i,BLOCK_SIZE((int)i/p_sqrt,p_sqrt,m_a),BLOCK_SIZE(i%p_sqrt,p_sqrt,n_a));
+#endif
+}
+
+if(!id){
+for (i=0;i<m_a;i++)
+  for (j=0;j<n_a;j++)
+   {fscanf(fin,"%f",&(a[i][j]));
+#ifdef DEBUG
+printf("%d %d IN %d*p_sqrt+%d=%d\n",i,j,BLOCK_OWNER(i,p_sqrt,m_a),BLOCK_OWNER(j,p_sqrt,n_a),BLOCK_OWNER(i,p_sqrt,m_a)*p_sqrt+BLOCK_OWNER(j,p_sqrt,n_a));
+//BLOCK_OWNER(i,p_sqrt,m_a)//recover p/p_sqrt
+//BLOCK_OWNER(j,p_sqrt,n_a)//recover p%p_sqrt
+//BLOCK_OWNER(i,p_sqrt,m_a)*p_sqrt+BLOCK_OWNER(j,p_sqrt,n_a)// recover p
+printf("GLOBAL ADDR %d %d LOCAL ADDR %d %d\n",i,j,i-BLOCK_LOW(BLOCK_OWNER(i,p_sqrt,m_a),p_sqrt,m_a),j-BLOCK_LOW(BLOCK_OWNER(j,p_sqrt,n_a),p_sqrt,n_a));
+#endif
+a_buffer[BLOCK_OWNER(i,p_sqrt,m_a)*p_sqrt+BLOCK_OWNER(j,p_sqrt,n_a)][i-BLOCK_LOW(BLOCK_OWNER(i,p_sqrt,m_a),p_sqrt,m_a)][j-BLOCK_LOW(BLOCK_OWNER(j,p_sqrt,n_a),p_sqrt,n_a)]=a[i][j];  
+}
+
+#ifdef DEBUG
+int k;
+if (!id)
+for (k=0;k<p;k++){
+printf("----%d-----\n",k);
+for (i=0;i<BLOCK_SIZE((int)k/p_sqrt,p_sqrt,m_a);i++)
+{
+for (j=0;j<BLOCK_SIZE(k%p_sqrt,p_sqrt,n_a);j++){printf("%f ", a_buffer[k][i][j]);}
+printf("\n");
+}
+}
+//print local matrix 
+#endif 
+}
+
+if (!id){
+a_chip=a_buffer[0];
+for (i=1;i<p;i++) MPI_Send(&(a_buffer[i][0][0]),BLOCK_SIZE((int)i/p_sqrt,p_sqrt,m_a)*BLOCK_SIZE(i%p_sqrt,p_sqrt,n_a),MPI_FLOAT,i,0,MPI_COMM_WORLD);
+}
+else {
+ MPI_Status status;
+ MPI_Recv(&a_chip[0][0],BLOCK_SIZE((int)id/p_sqrt,p_sqrt,m_a)*BLOCK_SIZE(id%p_sqrt,p_sqrt,n_a),MPI_FLOAT,0,0,MPI_COMM_WORLD,&status);
+}
+
+MPI_Barrier(MPI_COMM_WORLD);//------------------------------------------------------------------
+
+#ifdef DEBUG
+printf("\n-A-RECV-%d--\n",id);
+for (i=0;i<BLOCK_SIZE((int)id/p_sqrt,p_sqrt,m_a);i++){for (j=0;j<BLOCK_SIZE(id%p_sqrt,p_sqrt,n_a);j++)printf("%f ",a_chip[i][j]);printf("\n");} 
+#endif
+
+//------------------------------------------------------------------
+
+if (!id){fscanf(fin,"\n"); fscanf(fin,"%d %d\n",&m_b,&n_b);}
+
+MPI_Bcast(&m_b,1,MPI_INT,0,MPI_COMM_WORLD);
+MPI_Bcast(&n_b,1,MPI_INT,0,MPI_COMM_WORLD);
+
+MPI_Barrier(MPI_COMM_WORLD);
+
+if (MAX_PROCESS(MIN(m_a,n_a),MIN(m_b,n_b))<p_sqrt) {  
+   if (!id) {printf("\nFATAL ERROR: TOO MANY PROCESS\n");fflush(stdout);fclose(fin);}
+   MPI_Finalize();
+   free2dfloat(&a);
+   exit(1);
+}
+
+float **b;malloc2dfloat(&b,m_b,n_b);
+float **b_chip;malloc2dfloat(&b_chip,BLOCK_SIZE((int)id/p_sqrt,p_sqrt,m_b),BLOCK_SIZE(id%p_sqrt,p_sqrt,n_b));
+
+float **b_buffer[p];
+
+float **c;malloc2dfloat(&c,m_a,n_b);
+float **c_local;malloc2dfloat(&c_local,BLOCK_SIZE(id,p,m_a),n_b);
+
+
+if (n_a!=m_b) {  
+   if (!id) {printf("FATAL ERROR: N_A!=M_B\n");fflush(stdout);fclose(fin);}
+   MPI_Finalize();
+   free2dfloat(&a);free2dfloat(&b);free2dfloat(&c);free2dfloat(&c_local);
+   exit(1);
+}
+else 
+{ 
+if (!id) for (i=0;i<p;i++) malloc2dfloat(&b_buffer[i],BLOCK_SIZE((int)i/p_sqrt,p_sqrt,m_b),BLOCK_SIZE(i%p_sqrt,p_sqrt,n_b));//ALLOCATE BUFFER
+
+if(!id){
+for (i=0;i<m_b;i++)for (j=0;j<n_b;j++)
+{fscanf(fin,"%f",&(b[i][j]));
+b_buffer[BLOCK_OWNER(i,p_sqrt,m_b)*p_sqrt+BLOCK_OWNER(j,p_sqrt,n_b)][i-BLOCK_LOW(BLOCK_OWNER(i,p_sqrt,m_b),p_sqrt,m_b)][j-BLOCK_LOW(BLOCK_OWNER(j,p_sqrt,n_b),p_sqrt,n_b)]=b[i][j];}
+
+fclose(fin);
+}
+
+//MPI_Bcast(&(b[0][0]), m_b*n_b,MPI_FLOAT,0,MPI_COMM_WORLD);
+if (!id){
+b_chip=b_buffer[0];
+for (i=1;i<p;i++) MPI_Send(&(b_buffer[i][0][0]),BLOCK_SIZE((int)i/p_sqrt,p_sqrt,m_b)*BLOCK_SIZE(i%p_sqrt,p_sqrt,n_b),MPI_FLOAT,i,0,MPI_COMM_WORLD);
+}
+else {
+ MPI_Status status;
+ MPI_Recv(&b_chip[0][0],BLOCK_SIZE((int)id/p_sqrt,p_sqrt,m_b)*BLOCK_SIZE(id%p_sqrt,p_sqrt,n_b),MPI_FLOAT,0,0,MPI_COMM_WORLD,&status);
+}
+
+MPI_Barrier(MPI_COMM_WORLD);
+
+#ifdef DEBUG
+printf("\n-B-RECV-%d--\n",id);
+for (i=0;i<BLOCK_SIZE((int)id/p_sqrt,p_sqrt,m_b);i++){for (j=0;j<BLOCK_SIZE(id%p_sqrt,p_sqrt,n_b);j++)printf("%f ",b_chip[i][j]);printf("\n");}
+#endif
+
+MPI_Barrier(MPI_COMM_WORLD);
+//--BARRIER--
+
+for (i=0;i<BLOCK_SIZE(id,p,m_a);i++)
+for (j=0;j<n_b;j++){
+int k=0;
+float produit=0;
+for (k=0;k<n_a;k++) produit+=a_local[i][k]*b[k][j];
+c_local[i][j]=produit;
+}
+//
+#ifdef DEBUG
+printf("INFO C %d %d %d\n",id,m_a,n_b);
+#endif
+if (!id){
+ MPI_Status status;
+for (i=0;i<BLOCK_SIZE(0,p,m_a);i++) for (j=0;j<n_b;j++) c[i][j]=c_local[i][j];
+#ifdef DEBUG
+printf("PROCESS 0 COPY C_LOCAL TO C\n");
+#endif
+for (i=1;i<p;i++)
+ MPI_Recv(&(c[BLOCK_LOW(i,p,m_a)][0]),BLOCK_SIZE(i,p,m_a)*n_b,MPI_FLOAT,i,0,MPI_COMM_WORLD,&status);
+}
+else {
+MPI_Send(&(c_local[0][0]),BLOCK_SIZE(id,p,m_a)*n_b,MPI_FLOAT,0,0,MPI_COMM_WORLD);
+}
+#ifdef DEBUG
+printf("END PROCESS %d\n",id);
+#endif
+}
+
+if (!id) for (i=0;i<m_a;i++){for (j=0;j<n_b;j++){printf("%f ", c[i][j]);}printf("\n");}
+elapsed_time+=MPI_Wtime();
+if (!id) {printf("EXECUTION TIME (s): %10.6f\n",elapsed_time);fflush(stdout);}
+
+ MPI_Finalize();
+#ifdef DEBUG
+if (!id) {printf("\nMONO\n");mono(a,m_a,n_a,b,m_b,n_b);}
+#endif
+ free2dfloat(&a);free2dfloat(&b);free2dfloat(&c);free2dfloat(&a_local);free2dfloat(&c_local);
+ return 0;
+}
+
+void mono(float **a, int m_a,int n_a, float **b,int m_b,int n_b)
+{
+int i,j,k;
+float **c_mono;
+malloc2dfloat(&c_mono,m_a,n_b);
+for (i=0;i<m_a;i++)
+  for (j=0;j<n_b;j++)
+     c_mono[i][j]=0;
+for (i=0;i<m_a;i++)
+  for (j=0;j<n_b;j++)
+    for (k=0;k<n_a;k++)
+     c_mono[i][j]+=a[i][k]*b[k][j];
+for (i=0;i<m_a;i++){for (j=0;j<n_b;j++){printf("%f ", c_mono[i][j]);}printf("\n");}
+}
+
+int malloc2dfloat(float ***array, int n, int m) {    
+    float *p = (float *)malloc(n*m*sizeof(float));
+    if (!p) return -1;
+
+    (*array) = (float **)malloc(n*sizeof(float*));
+    if (!(*array)) {
+       free(p);
+       return -1;
+    }
+
+    for (int i=0; i<n; i++) 
+       (*array)[i] = &(p[i*m]);
+
+    return 0;
+}
+
+int free2dfloat(float ***array) {free(&((*array)[0][0]));free(*array);return 0;}
